b5297971564ded3ec7af847f314f0390baa57cc4
Alon Djurinsky <alon.djurinsky@gmail.com>
2020-12-23 16:22:28 -0500

    some helper code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 #include <array>
+#include <vector>
 
 #define coordinate std::array<char, 2>
+#define piece unsigned char
+
+// Utility functions to deal with the colors of pieces
+#define WHITE_BITMASK 0b10000000
+inline bool isWhite(piece p)
+{
+    return (p & WHITE_BITMASK) > 0;
+}
+inline void setWhite(piece &p)
+{
+    p |= WHITE_BITMASK;
+}
+inline void setBlack(piece &p)
+{
+    p &= ~WHITE_BITMASK;
+}
+
+piece PAWN = 'p';
+piece BISHOP = 'b';
+piece KNIGHT = 'n';
+piece ROOK = 'r';
+piece QUEEN = 'q';
+piece KING = 'k';
 
 // Some constants for accessing information from State and Move classes
 const int KINGSIDE = 0;
@@ -23,6 +47,13 @@ public:
 
     bool enPassantAvailable;      // If a length 2 pawn move was made last turn
     coordinate enPassantLocation; // Which square can be taken via en passant
+
+    std::vector<State> allPossibleMoves() const
+    {
+        std::vector<State> allMoves{};
+
+        return allMoves;
+    }
 };
 
 class Move
@@ -33,9 +64,26 @@ public:
     coordinate from;
     coordinate to;
 
+    bool castleKingside = false;
+    bool castleQueenside = false;
+
     Move() = delete;
     Move(std::string ean)
     {
+        if (ean.find("O-O") == 0)
+        {
+            if (ean.find("O-O-O"))
+            {
+                castleQueenside = true;
+                return;
+            }
+            else
+            {
+                castleKingside = true;
+                return;
+            }
+        }
+
         from = {(char)(ean[0] - 'a'), (char)(ean[1] - '1')};
         to = {(char)(ean[2] - 'a'), (char)(ean[3] - '1')};
     }
